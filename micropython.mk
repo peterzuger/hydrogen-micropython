6d3646b85e39ac2553d386f619b6ee3d0bd29c85
Peter Züger <zueger.peter@icloud.com>
2021-05-31 21:06:42 +0200

    added workaround for GCC11 array bound warnings

--- micropython.mk
+++ micropython.mk
@@ -4,6 +4,9 @@ HYDROGEN_MOD_DIR := $(USERMOD_DIR)
 SRC_USERMOD += $(HYDROGEN_MOD_DIR)/hydrogen.c
 SRC_C += lib/libhydrogen/hydrogen.c
 
+# workaround for GCC11 array bound warnings
+CFLAGS += -Wno-stringop-overflow -Wno-stringop-overread
+
 # If not compiling for unix
 ifdef BOARD
 CFLAGS += -I$(HYDROGEN_MOD_DIR) -DPARTICLE -DPLATFORM_ID=3
