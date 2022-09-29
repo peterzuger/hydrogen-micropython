3fce34dc05fe9caa20f45c9111b41e00e5185ba9
Peter Züger <zueger.peter@icloud.com>
2022-09-29 23:20:41 +0200

    fixed micropython.cmake for esp32

--- micropython.cmake
+++ micropython.cmake
@@ -9,20 +9,17 @@ target_include_directories(usermod_hydrogen INTERFACE
   ${CMAKE_CURRENT_LIST_DIR}
 )
 
-# hydrogen does not need any workarounds for esp32
-if(NOT IDF_TARGET STREQUAL "esp32")
-  target_compile_definitions(usermod_hydrogen INTERFACE
-    PARTICLE
-    PLATFORM_ID=3
-  )
-
-  # set compile flags for QSTR generation
-  set(MICROPY_CPP_FLAGS_EXTRA "-DPARTICLE" "-DPLATFORM_ID=3")
-endif()
+target_compile_definitions(usermod_hydrogen INTERFACE
+  PARTICLE
+  PLATFORM_ID=3
+)
 
 target_compile_options(usermod_hydrogen INTERFACE
   -Wno-stringop-overflow
   -Wno-stringop-overread
 )
+
+# set compile flags for QSTR generation
+list(APPEND MICROPY_CPP_FLAGS_EXTRA "-DPARTICLE" "-DPLATFORM_ID=3")
 
 target_link_libraries(usermod INTERFACE usermod_hydrogen)
