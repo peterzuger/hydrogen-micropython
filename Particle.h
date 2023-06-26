df6d5bfdf67ce9369de90f12e873ffe367741ee8
Peter Züger <zueger.peter@icloud.com>
2023-06-26 13:36:55 +0200

    updated URANDOM to RANDOM
    for micropython/micropython#9069
    handle MICROPY_PY_RANDOM_SEED_INIT_FUNC not being defined

--- Particle.h
+++ Particle.h
@@ -3,7 +3,12 @@
 // verifying a signature, but a correct implementation is provided anyway.
 
 #include "py/mphal.h"
+#include "py/runtime.h"
 
 static inline uint32_t HAL_RNG_GetRandomNumber(void) {
-    return MICROPY_PY_URANDOM_SEED_INIT_FUNC;
+    #ifdef MICROPY_PY_RANDOM_SEED_INIT_FUNC
+    return MICROPY_PY_RANDOM_SEED_INIT_FUNC;
+    #else
+    #error MICROPY_PY_RANDOM_SEED_INIT_FUNC not defined
+    #endif /* MICROPY_PY_RANDOM_SEED_INIT_FUNC */
 }
